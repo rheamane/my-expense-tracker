20a732dd7e6aa923d54a5d7c883fec9d77f5ee0e
Munachimso Aneke <52995219+anekemuna@users.noreply.github.com>
2025-07-12 17:34:39 -0700

    Add card component

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,22 +1,32 @@
-// import {
-//   Card,
-//   CardAction,
-//   CardContent,
-//   CardDescription,
-//   CardFooter,
-//   CardHeader,
-//   CardTitle,
-// } from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Form() {
   return (
-    // <>
-    // <Card>
-    //     <CardHeader></CardHeader>
-
-    // </Card>
-
-    // </>
-    <div>Hello</div>
+    <>
+      <Card className="w-full max-w-lg">
+        <CardHeader>
+          <CardTitle>Add Expense</CardTitle>
+          <CardDescription> Card Description</CardDescription>
+          <CardAction>
+            <Button>X</Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <p>CardContent</p>
+        </CardContent>
+        <CardFooter>
+          <Button>Submit</Button>
+        </CardFooter>
+      </Card>
+    </>
   );
 }
