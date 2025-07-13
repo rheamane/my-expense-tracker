a6c5bdb82334daeab33f10250a5492a32b94c39e
Munachimso Aneke <52995219+anekemuna@users.noreply.github.com>
2025-07-12 20:20:55 -0700

    Add Form as a Dialog popup
    Form made into expenseForm.tsx component
    Dialog in app/layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,37 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import ExpenseForm from "@/components/expenseForm";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 
 export default function Home() {
   return (
-    <Button asChild>
-      <Link href="/form">Add Expense</Link>
-    </Button>
+    <>
+      {/* <Button asChild>
+        <Link href="/form">Add Expense</Link>
+      </Button> */}
+      <div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>Add Expense</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Add New Expense</DialogTitle>
+              <DialogDescription asChild>
+                <ExpenseForm />
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
+    </>
   );
 }
