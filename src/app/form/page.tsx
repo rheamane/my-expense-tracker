3fc5a7b06814de8ec8af16dd5acda3071d0f0b2b
Rhea Mane <95063614+orangejuice001@users.noreply.github.com>
2025-07-12 17:05:22 -0700

    changes
    Co-authored-by: Munachimso Aneke <anekemuna@users.noreply.github.com>

---
+++ src/app/form/page.tsx
@@ -0,0 +1,22 @@
+// import {
+//   Card,
+//   CardAction,
+//   CardContent,
+//   CardDescription,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle,
+// } from "@/components/ui/card"
+
+export default function Form() {
+  return (
+    // <>
+    // <Card>
+    //     <CardHeader></CardHeader>
+
+    // </Card>
+
+    // </>
+    <div>Hello</div>
+  );
+}
